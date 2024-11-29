4f43d5ec62f6859f31bf607e06f7f4de1f2f8c6b
Iulian <fp.idragoi@salesianosdosa.com>
2024-11-29 14:30:09 +0100

    punteros

---
+++ Programacion/Punteros/P6-Punteros.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+ typedef enum  {
+ 	ficcion,
+
+
+
+
+ 	
+ } genero literario;
+
+struct biblioteca {
+
+ int libro;
+ char titulo libro [100];
+ float precio;
+ int cantidad;
+ 
+
+
+
+
+
+
+	
+};
+
+
+
+int main (){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
